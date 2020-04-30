835228d3fde9d93d11de11b638c5e616ad605665
Matthew Leonard <leonard.matthewk@gmail.com>
2020-04-30 17:08:20 -0400

    creating databases and starting server.js

---
+++ Databases/schema.sql
@@ -0,0 +1,31 @@
+Drop DATABASE IF EXISTS Leonard_Supply_db;
+
+CREATE DATABASE Leonard_Supply_db;
+
+USE Leonard_Supply_db;
+
+CREATE TABLE Department{
+    
+    id INT NOT NULL AUTO_INCREMENT;
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+};
+
+CREATE TABLE role (
+
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL, 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
---
+++ Databases/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000.00, 1), ("Salesperson", 80000.00, 2), ("Lead Engineer", 150000.00, 3),("Software Engineer", 120000.00,4),("Accountant", 125000.00, 5), ("Legal Team Lead", 190000.00, 6), ("Lawyer", 120000.00, 7);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, null), ("Mike", "Chan", 2, "John Doe"), ("Ashley", "Rodriguez", 3, null), ("Kevin", "Tupik", 4, "Ashley Rodriguez"), ("Malia", "Brown", 6, null), ("Sarah", "Lourd", 7, null), ("Tom", "Allen", 8, "Sarah Lourde");
