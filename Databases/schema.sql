5db591eff080e78c5cbe8cd2401a6b4d81497f99
Matthew Leonard <leonard.matthewk@gmail.com>
2020-05-01 13:19:07 -0400

    setting up mysql tables with data and functions that will run during the tracker

--- Databases/schema.sql
+++ Databases/schema.sql
@@ -4,15 +4,13 @@ CREATE DATABASE Leonard_Supply_db;
 
 USE Leonard_Supply_db;
 
-CREATE TABLE Department{
-    
-    id INT NOT NULL AUTO_INCREMENT;
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-};
+);
 
 CREATE TABLE role (
-
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
@@ -21,7 +19,6 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
--- server.js
+++ server.js
@@ -1,23 +1,91 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require("console.table");
+// // const cTable = require("console.table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    port: 3306,
-  
-    user: "root",
-  
-    password: "",
-    database: "Leonard_Supply_db"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    startTracker();
-  });
+  host: "localhost",
 
-  function startTracker(){
+  port: 3306,
 
-  }
+  user: "root",
+
+  password: "",
+  database: "Leonard_Supply_db",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  startTracker();
+});
+
+function startTracker() {
+  inquirer
+    .prompt({
+      type: list,
+      choices: [
+        "Add departments",
+        "Add roles",
+        "Add employees",
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update employee roles",
+        "Update employee managers",
+        "View employees by manager",
+        "Remove departments",
+        "Remove roles",
+        "Remove employees",
+        "View total department budget",
+        "Exit",
+      ],
+      message: "What option would you like to select?",
+      name: "option",
+    })
+    .then(function (answer) {
+      console.log("You have selected: " + answer.option);
+
+      switch (answer.option) {
+        case "Add departments":
+          addDepartment();
+          break;
+        case "Add roles":
+          addRoles();
+          break;
+        case "Add employess":
+          addEmployees();
+          break;
+        case "View departments":
+          viewDepartment();
+          break;
+        case "View roles":
+          viewRoles();
+          break;
+        case "View employess":
+          viewEmployees();
+          break;
+        case "Update employee roles":
+          updateEmployeeRoles();
+          break;
+        case "Update employee managers":
+          updateEmployeeManagers();
+          break;
+        case "View employees by manager":
+          viewEmployeesByManager();
+          break;
+        case "Remove departments":
+          removeDepartments();
+          break;
+        case "Remove roles":
+          removeRoles();
+          break;
+        case "Remove employees":
+          removeEmployees();
+          break;
+        case "View total department budget":
+          viewTotalDepartmentBudget();
+          break;
+        case "Exit":
+          exit();
+      }
+    });
+}
